ebfd91a5dcb00f58bc416ef5df2bdbffc4237db2
zikrulloabdumalikov-debug <zikrulloabdumalikov@gmail.com>
2026-01-25 01:14:17 +0500

    refactor: Update index.html and vite.config.ts
    Removes unused CSS and updates Vite base configuration.
    Introduces minor changes to React DOM rendering for improved robustness.

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // SHU QATORNI QO'SHING:
-      base: '/million-km-/', 
-      
       server: {
         port: 3000,
         host: '0.0.0.0',
