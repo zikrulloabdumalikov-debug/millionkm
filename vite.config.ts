7174175a3d96d9805c7111d97fc55d9ded95781c
zikrulloabdumalikov-debug <zikrulloabdumalikov@gmail.com>
2026-01-25 02:58:05 +0500

    Update base path in Vite configuration
    Signed-off-by: zikrulloabdumalikov-debug <zikrulloabdumalikov@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/million-km-/', // Repozitoriya nomi bilan bir xil bo'lishi shart
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
+  base: '/millionkm/',
 });
