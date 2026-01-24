79e345210a644eac909cfc7cd0ef9952279a86c6
zikrulloabdumalikov-debug <zikrulloabdumalikov@gmail.com>
2026-01-25 01:26:29 +0500

    feat: Configure Vite for GitHub Pages deployment
    Update `index.html` and `vite.config.ts` to correctly configure the Vite build for deployment on GitHub Pages. This includes setting the correct base path, ensuring the `index.html` is correctly referenced in the build output, and updating the import order of Vite plugins for consistency.

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/million-km-/', // GitHub repozitoriyangiz nomiga mos bo'lishi shart
+  base: '/million-km-/', // Repozitoriya nomi bilan bir xil bo'lishi shart
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
     assetsDir: 'assets',
-    sourcemap: false
-  }
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 });
