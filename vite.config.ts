27b5e1c5fd10546cf3f5dd63b81e25c81764c208
zikrulloabdumalikov-debug <zikrulloabdumalikov@gmail.com>
2026-03-04 13:14:05 +0500

    Set base path for Vite configuration
    Signed-off-by: zikrulloabdumalikov-debug <zikrulloabdumalikov@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
