01f91e8af15de604973c8a583a7e1d676d413b81
zikrulloabdumalikov-debug <zikrulloabdumalikov@gmail.com>
2026-03-04 14:59:42 +0500

    Update data.js
    Signed-off-by: zikrulloabdumalikov-debug <zikrulloabdumalikov@gmail.com>

--- js/data.js
+++ js/data.js
@@ -30,19 +30,23 @@ export const BRAND_LOGOS = {
 };
 
 export const sendTelegramNotification = async (message) => {
-    const TG_TOKEN = "7722483735:AAG_LZ1Bg0H-mnqAlnw4OknNj-BTrqM8CWM";
-    const TG_CHAT_ID = "-1003461463026";
-    const url = `https://api.telegram.org/bot${TG_TOKEN}/sendMessage`;
-    try {
+  const TG_TOKEN = "7722483735:AAG_LZ1Bg0H-mnqAlnw4OknNj-BTrqM8CWM";
+  const TG_CHAT_ID = "-1003461463026";
+  const url = `https://api.telegram.org/bot${TG_TOKEN}/sendMessage`;
+  try {
     const response = await fetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        chat_id: window.TG_CHAT_ID,
+        chat_id: TG_CHAT_ID,
         text: message,
         parse_mode: 'HTML'
       })
     });
+    const result = await response.json();
+    if (!response.ok) {
+      console.error("Telegram API error:", result);
+    }
     return response.ok;
   } catch (error) {
     console.error("Telegram Error:", error);
