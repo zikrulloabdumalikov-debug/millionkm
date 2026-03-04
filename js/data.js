ab8fe09b62a6c6b81b57de8f7064e1ba728a490d
zikrulloabdumalikov-debug <zikrulloabdumalikov@gmail.com>
2026-03-04 14:50:05 +0500

    Update data.js
    Signed-off-by: zikrulloabdumalikov-debug <zikrulloabdumalikov@gmail.com>

--- js/data.js
+++ js/data.js
@@ -30,8 +30,9 @@ export const BRAND_LOGOS = {
 };
 
 export const sendTelegramNotification = async (message) => {
-  const url = `https://api.telegram.org/bot${window.TG_TOKEN}/sendMessage`;
-  try {
+  const TG_TOKEN = "7722483735:AAG_LZ1Bg0H-mnqAlnw4OknNj-BTrqM8CWM";
+const TG_CHAT_ID = "-1003461463026";
+const url = `https://api.telegram.org/bot${TG_TOKEN}/sendMessage`;
     const response = await fetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
